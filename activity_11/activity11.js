5e8f0b12b59d4a1c7c0cba9b172d4a2fe38c5b9b
TaranS <73401526+Tarandeepsingh436@users.noreply.github.com>
2022-08-03 19:57:35 -0400

    Update activity11.js

--- activity_11/activity11.js
+++ activity_11/activity11.js
@@ -2,7 +2,7 @@ console.log("Hello World");
 
 
 
-//ex 2 
+//Ex 2 
 const btn2 = document.querysSelector("#btn2");
 btn2.onclick = function(){
     alert('Hi There!')
@@ -11,26 +11,26 @@ btn2.onclick = function(){
 
 /* another way to create a function */
 
-//ex 3 
+//Ex 3 
 const qcclink = document.querySelector("#qcclink");
 qcclink.onclick = function(){
-    console.log("qcc link was clicked");
+    console.log("QCC link was clicked");
 }
 
 qcclink.onmouseover = testing;
 function testing(){
-    console.log("qcc link was hovered");
+    console.log("Qcc link was hover over");
 }
 
 
-///example 4
+///Ex 4
 const title = document.querySelector(`.title`)
 title.onmouseover = function(){
     console.log("TH e title was hovered on mouseout event");
 }
 
 
-//ex 5 
+//Ex 5 
 const btn5 = document.querySelector("#btn5");
 btn5.addEventListener('click', function(){
     alert('Button 5 was clicked')
@@ -39,7 +39,7 @@ btn5.addEventListener('click', function(){
 
 
 
-//ex 6
+//Ex 6
 const btn6 = document.querySelector("#btn6");
 btn6.addEventListener('mouseout',click2)
 btn6.addEventListener('dblclick',click1)
@@ -71,7 +71,7 @@ function changeColor(){
 
 
 
-// ex 9
+// Ex 9
 const btn9 = document.querySelector("#btn9");
 for(let eachDiv of divColor){
     eachDiv.addEvenListner('mouseout', function(){
@@ -80,19 +80,64 @@ for(let eachDiv of divColor){
 }
 
 
-// ex 10
+// Ex 10
 const inputTxt = document.querySelector(".inputTxt");
 inputTxt.addEventListener('keyup', function(){
     alert('KEYDOWN! key "${e.key}" was pressed \n The ASCII code for key "${e.key}" is ${e.which} ')
 })
 
 
+// ex 11
+const display9 = document.querySelector('.display9');
+window.addEventListener('scroll', function(){
+    let pxTop = window.pageYOffset;
+    display9.innerHTMl = `Browser window is ${pxTop} px away from the top`
+})
+
+// ex 12
+const toTop = document.querySelector('.toTop');
+window.addEventListener('scroll', function(){
+    let pzTop = window.pageYOffset;
+    if(pzTop > 80){
+        toTop.style.display = 'block';
+    }
+    else{
+        toTop.style.display = 'none';
+    }
+})
 
 
 
 
 
+// ex 13 
+const qccURL = document.querySelector('.qccURL')
+qccURL.addEventListener('click', function(){
+    e.preventDefault();
+    alert("QCC  website  is OFF")
+})
 
+
+
+// ex 14 
+const  sBubble = document.quesrySelector('.sBubble');
+const pBubbble = document.querySelector('.pBubble');
+const aBubble = document.querySelector('.aBubble');
+
+sBubble.addEventListener('click', function(){
+    e.stopPropagation();
+    alert('Section  was clicked')
+})
+
+pBubbble.addEventListener('click', function(){
+    e.stopPropagation();
+    alert('PARAGRAPH was clicked')
+})
+
+aBubble.addEventListener('click', function(){
+    e.stopPropagation();
+    alert('Anchor was clicked')
+})
 
 
 
